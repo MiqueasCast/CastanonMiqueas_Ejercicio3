971f425538f233d9f17bae5a582f667aeda9c45f
MiqueasCast <castmicah@gmail.com>
2026-02-22 14:42:59 -0600

    integración de ifs

--- CastanonMiqueas_Ejercicio3/Program.cs
+++ CastanonMiqueas_Ejercicio3/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("Seleccione según su rol:\n1. Estudiante \n2. Docente \n3. Administrativo \n4. Externo");
+﻿using System.Collections.Specialized;
+using System.ComponentModel.Design;
+
+Console.WriteLine("Seleccione según su rol:\n1. Estudiante \n2. Docente \n3. Administrativo \n4. Externo");
 int rol = int.Parse(Console.ReadLine());
 
 Console.WriteLine("Ingrese monto base: ");
@@ -9,15 +12,58 @@ int paymentMethod = int.Parse(Console.ReadLine());
 
 Console.WriteLine("Tiene cupón S/N: ");
 string hasCoupon = Console.ReadLine();
-
+string couponCode =  "";
 if (hasCoupon == "S" || hasCoupon == "s")
 {
     Console.WriteLine("Ingrese codigo: ");
-    string couponCode = Console.ReadLine();
+    couponCode = Console.ReadLine();
+
+}
+
+Console.WriteLine("Reporte antifraude: \n1. Ninguno \n. Cupón inválido repetido \n3. Pagos rechazados múltiples");
+int antifraudReport = int.Parse(Console.ReadLine());
+
+if (totalAmount <= 0 || rol < 1 || rol > 4 || paymentMethod < 1 || paymentMethod > 3 || antifraudReport < 1 || antifraudReport > 3)
+{
+    Console.WriteLine("Datos inválidos");
 }
 else
 {
-    Console.WriteLine("No tiene cupón");
-}
+    double discount = 0;
+    double recargo = 0;
 
+    if (antifraudReport != 1)
+    {
+        if (antifraudReport == 2)
+        {
+            recargo = 0.10;
+            Console.WriteLine("Fraude tipo 2. Se cancelan descuentos y se aplicara un recargo de 10%");
+        }
+        else if (antifraudReport == 3)
+        {
+            recargo = 0.15;
+            Console.WriteLine("Fraude tipo 3. Se cancelan descuentos y se aplica recargo del 20%");
+        }
+    }
+    else
+    {
+        if (rol == 1 && paymentMethod == 1)
+        {
+            discount = 0.15;
+        }
+        else if (rol == 2 && paymentMethod == 2)
+        {
+            discount = 0.10;
+        }
+
+        if (hasCoupon == "s" || hasCoupon == "S" && couponCode.StartsWith("U"))
+        {
+            char lastChar = couponCode[couponCode.Length - 1];
+            if (lastChar == '0' || lastChar == '2' || lastChar == '4' || lastChar == '6' || lastChar == '8')
+            {
+                discount += 0.05;
+            }
+        }
+    }
+}
 
